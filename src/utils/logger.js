66b3c27e8e6b41050d164237eec34c50085b3277
Chrysler-Lexus <kenkaneky244@gmail.com>
2026-04-20 08:20:24 +0100

    logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,14 +2,16 @@ const pino = require('pino');
 
 const logger = pino({
     level: 'info',
-    transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true,
-            translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
-            ignore: 'pid,hostname'
+    ...(process.env.NODE_ENV !== 'production' && {
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                colorize: true,
+                translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
+                ignore: 'pid,hostname'
+            }
         }
-    }
+    })
 });
 
 module.exports = logger;
