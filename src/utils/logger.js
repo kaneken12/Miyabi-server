32a3f7023d2a7e46e92d5653b05bab0275eefa3c
Chrysler-Lexus <kenkaneky244@gmail.com>
2026-04-24 15:12:39 +0100

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,17 +1,7 @@
 const pino = require('pino');
 
 const logger = pino({
-    level: 'info',
-    ...(process.env.NODE_ENV !== 'production' && {
-        transport: {
-            target: 'pino-pretty',
-            options: {
-                colorize: true,
-                translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
-                ignore: 'pid,hostname'
-            }
-        }
-    })
+    level: 'info'
 });
 
 module.exports = logger;
